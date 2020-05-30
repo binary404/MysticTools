5a14bc8437d3c29a5a3bcd1912485bf333251eb4
CSchlaud04 <35374899+CSchlaud04@users.noreply.github.com>
2020-05-30 16:21:34 -0500

    Minor change

--- src/main/java/binary404/mystictools/common/loot/effects/IUniqueEffect.java
+++ src/main/java/binary404/mystictools/common/loot/effects/IUniqueEffect.java
@@ -2,6 +2,7 @@ package binary404.mystictools.common.loot.effects;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.world.World;
 
 public interface IUniqueEffect {
@@ -11,5 +12,9 @@ public interface IUniqueEffect {
     void hit(LivingEntity target, LivingEntity attacker, ItemStack stack);
 
     void rightClick(LivingEntity entity, ItemStack stack);
+
+    CompoundNBT write(CompoundNBT nbt, LivingEntity entity);
+
+    void read(CompoundNBT nbt, LivingEntity entity);
 
 }
