9d0da39cb413fc6003d737bbe44ee403c2398770
CSchlaud04 <35374899+CSchlaud04@users.noreply.github.com>
2020-07-26 13:25:55 -0500

    oopsy

---
+++ src/main/java/binary404/mystictools/common/blocks/BlockCauldron.java
@@ -0,0 +1,84 @@
+package binary404.mystictools.common.blocks;
+
+import binary404.mystictools.common.core.UniqueHandler;
+import binary404.mystictools.common.items.ILootItem;
+import binary404.mystictools.common.items.ModItems;
+import binary404.mystictools.common.loot.LootItemHelper;
+import binary404.mystictools.common.loot.LootNbtHelper;
+import binary404.mystictools.common.loot.LootRarity;
+import binary404.mystictools.common.loot.LootTags;
+import binary404.mystictools.common.network.NetworkHandler;
+import binary404.mystictools.common.network.PacketOpenCrateFX;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.IBooleanFunction;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import sun.security.provider.SHA;
+
+import javax.annotation.Nullable;
+
+public class BlockCauldron extends Block {
+
+    private static final VoxelShape INSIDE = makeCuboidShape(2.0D, 8.0D, 2.0D, 14.0D, 16.0D, 14.0D);
+    protected static final VoxelShape SHAPE = VoxelShapes.combineAndSimplify(VoxelShapes.fullCube(), VoxelShapes.or(makeCuboidShape(0.0D, 0.0D, 4.0D, 16.0D, 3.0D, 12.0D), makeCuboidShape(4.0D, 0.0D, 0.0D, 12.0D, 3.0D, 16.0D), makeCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 3.0D, 14.0D), INSIDE), IBooleanFunction.ONLY_FIRST);
+
+    public BlockCauldron(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
+        if (!worldIn.isRemote && entityIn instanceof ItemEntity) {
+            ItemEntity itemEntity = (ItemEntity) entityIn;
+            Item item = itemEntity.getItem().getItem();
+            if (item instanceof ILootItem) {
+                ItemStack stack;
+                LootRarity rarity = LootRarity.fromId(LootNbtHelper.getLootStringValue(itemEntity.getItem(), LootTags.LOOT_TAG_RARITY));
+                if (rarity == LootRarity.COMMON) {
+                    stack = new ItemStack(ModItems.shard, worldIn.rand.nextInt(5));
+                } else if (rarity == LootRarity.UNCOMMON) {
+                    stack = new ItemStack(ModItems.shard, worldIn.rand.nextInt(7));
+                } else if (rarity == LootRarity.RARE) {
+                    stack = new ItemStack(ModItems.shard, worldIn.rand.nextInt(10));
+                } else if (rarity == LootRarity.EPIC) {
+                    stack = new ItemStack(ModItems.shard, worldIn.rand.nextInt(15));
+                } else if (rarity == LootRarity.UNIQUE) {
+                    stack = new ItemStack(ModItems.shard, worldIn.rand.nextInt(20));
+                    if (worldIn instanceof ServerWorld)
+                        UniqueHandler.resetUniqueItems((ServerWorld) worldIn);
+                } else {
+                    stack = new ItemStack(ModItems.shard, worldIn.rand.nextInt(5));
+                }
+                NetworkHandler.sendToNearby(worldIn, entityIn, new PacketOpenCrateFX(entityIn.getPosX(), entityIn.getPosY(), entityIn.getPosZ()));
+                itemEntity.setItem(stack);
+            }
+        }
+    }
+
+    @Override
+    public boolean hasTileEntity(BlockState state) {
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
+        return new TileEntityCauldron();
+    }
+}
---
+++ src/main/java/binary404/mystictools/common/loot/effects/LootEffect.java
@@ -0,0 +1,176 @@
+package binary404.mystictools.common.loot.effects;
+
+import binary404.mystictools.common.loot.LootItemHelper;
+import binary404.mystictools.common.loot.LootNbtHelper;
+import binary404.mystictools.common.loot.LootSet;
+import binary404.mystictools.common.loot.LootTags;
+import binary404.mystictools.common.loot.effects.effect.LootEffectAreaMiner;
+import binary404.mystictools.common.loot.effects.effect.LootEffectAutoSmelt;
+import binary404.mystictools.common.loot.effects.effect.LootEffectVoid;
+import com.sun.org.apache.regexp.internal.RE;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.storage.loot.LootEntry;
+
+import java.util.*;
+
+public class LootEffect {
+    public static final Map<String, LootEffect> REGISTRY = new HashMap<>();
+
+
+    public static final LootEffect LEECH = create("leech", EffectType.PASSIVE).setAmplifier(1, 100).setItemTypes(LootSet.LootSetType.SWORD);
+
+    public static final LootEffect MULTI = create("multi", EffectType.PASSIVE).setItemTypes(LootSet.LootSetType.AXE, LootSet.LootSetType.SHOVEL, LootSet.LootSetType.PICKAXE);
+    public static final LootEffect AREA_MINER = create("area_miner", EffectType.ACTIVE).setItemTypes(LootSet.LootSetType.PICKAXE, LootSet.LootSetType.SHOVEL, LootSet.LootSetType.AXE).setAction(new LootEffectAreaMiner());
+    public static final LootEffect AUTO_SMELT = create("auto_smelt", EffectType.ACTIVE).setItemTypes(LootSet.LootSetType.PICKAXE, LootSet.LootSetType.SHOVEL, LootSet.LootSetType.AXE).setAction(new LootEffectAutoSmelt());
+    public static final LootEffect VOID = create("void", EffectType.ACTIVE).setItemTypes(LootSet.LootSetType.PICKAXE, LootSet.LootSetType.AXE, LootSet.LootSetType.SHOVEL).setAction(new LootEffectVoid());
+
+
+    private String id;
+    private List<LootSet.LootSetType> applyToItems = new ArrayList<>();
+
+    private IEffectAction action;
+
+    private int amplifierMin = 0;
+    private int amplifierMax = 0;
+
+    private final EffectType effectType;
+
+    public enum EffectType {
+        ACTIVE(TextFormatting.GOLD),
+        PASSIVE(TextFormatting.GREEN),
+        USE(TextFormatting.AQUA);
+
+        private TextFormatting color;
+
+        private EffectType(TextFormatting color) {
+            this.color = color;
+        }
+
+        public TextFormatting getColor() {
+            return this.color;
+        }
+
+        public boolean equals(EffectType type) {
+            return type == this;
+        }
+    }
+
+    private LootEffect(EffectType type) {
+        this.effectType = type;
+    }
+
+    public EffectType getType() {
+        return this.effectType;
+    }
+
+    public boolean applyToItemType(LootSet.LootSetType type) {
+        return applyToItems.contains(type);
+    }
+
+    public IEffectAction getAction() {
+        return this.action;
+    }
+
+    protected LootEffect setAction(IEffectAction action) {
+        this.action = action;
+        return this;
+    }
+
+    protected LootEffect setItemTypes(LootSet.LootSetType... itemTypes) {
+        for (LootSet.LootSetType itemType : itemTypes) {
+            applyToItems.add(itemType);
+        }
+        return this;
+    }
+
+    public static LootEffect getById(String id) {
+        LootEffect weaponEffect = REGISTRY.get(id);
+
+        return weaponEffect;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public CompoundNBT getNbt(Random rand) {
+        CompoundNBT tag = new CompoundNBT();
+
+        tag.putString("id", this.getId());
+        tag.putInt("amplifier", this.getAmplifier(rand));
+        return tag;
+    }
+
+    public static int getAmplifierFromStack(ItemStack stack, String effectId) {
+        int amplifier = 0;
+
+        ListNBT effectTagList = LootNbtHelper.getLootTagList(stack, LootTags.LOOT_TAG_EFFECTLIST);
+
+        int count = effectTagList.size();
+
+        for (int i = 0; i < count; ++i) {
+            CompoundNBT e = effectTagList.getCompound(i);
+            if (e.getString("id").contains(effectId)) {
+                amplifier = e.getInt("amplifier");
+            }
+        }
+
+        return amplifier;
+    }
+
+    public static List<LootEffect> getEffectList(ItemStack stack) {
+        List<LootEffect> list = new ArrayList<>();
+
+        ListNBT effectList = LootNbtHelper.getLootTagList(stack, LootTags.LOOT_TAG_EFFECTLIST);
+
+        int count = effectList.size();
+
+        for (int i = 0; i < count; ++i) {
+            CompoundNBT e = effectList.getCompound(i);
+            list.add(LootEffect.getById(e.getString("id")));
+        }
+
+        return list;
+    }
+
+    protected static LootEffect create(String id, EffectType type) {
+        LootEffect effect = new LootEffect(type);
+
+        effect.id = id;
+        REGISTRY.put(id, effect);
+        return effect;
+    }
+
+    protected LootEffect setAmplifier(int min, int max) {
+        this.amplifierMin = min;
+        this.amplifierMax = max;
+        return this;
+    }
+
+    private int getAmplifier(Random rand) {
+        return MathHelper.nextInt(rand, this.amplifierMin, this.amplifierMax);
+    }
+
+    public String getActionStatus(ItemStack stack) {
+        String statusString = "";
+
+        if (this.getAction() != null) {
+            statusString = this.getAction().getStatusString(stack);
+        }
+
+        return statusString;
+    }
+
+    public String getAmplifierString(ItemStack stack, String effectId) {
+        return getAmplifierString(stack, effectId, 0);
+    }
+
+    public String getAmplifierString(ItemStack stack, String effectId, int add) {
+        return TextFormatting.BOLD + "" + (getAmplifierFromStack(stack, effectId) + add) + "" + TextFormatting.RESET + "" + ((this.getType() == EffectType.PASSIVE) ? TextFormatting.GREEN : TextFormatting.GOLD) + "";
+    }
+
+}
