f47af49175ec60db356492dbc706f2e661c25c14
CSchlaud04 <35374899+CSchlaud04@users.noreply.github.com>
2020-07-30 07:50:13 -0500

    Recipes

--- src/main/java/binary404/mystictools/data/RecipeProvider.java
+++ src/main/java/binary404/mystictools/data/RecipeProvider.java
@@ -2,6 +2,7 @@ package binary404.mystictools.data;
 
 import binary404.mystictools.common.blocks.ModBlocks;
 import binary404.mystictools.common.items.ModItems;
+import net.minecraft.block.Blocks;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.ShapedRecipeBuilder;
@@ -31,6 +32,37 @@ public class RecipeProvider extends net.minecraft.data.RecipeProvider {
                 .patternLine("III")
                 .setGroup("mystictools:cauldron")
                 .addCriterion("has_item", hasItem(ModBlocks.cauldron))
+                .build(consumer);
+        ShapedRecipeBuilder.shapedRecipe(ModBlocks.upgrader)
+                .key('A', Blocks.ANVIL)
+                .key('I', Tags.Items.INGOTS_IRON)
+                .key('S', ModItems.shard)
+                .key('D', Tags.Items.GEMS_DIAMOND)
+                .patternLine("IAI")
+                .patternLine("SDS")
+                .patternLine("IDI")
+                .setGroup("mystictools:upgrader")
+                .addCriterion("has_item", hasItem(ModItems.shard))
+                .build(consumer);
+        ShapedRecipeBuilder.shapedRecipe(ModItems.charm)
+                .key('S', ModItems.shard)
+                .key('E', Tags.Items.GEMS_EMERALD)
+                .key('C', ModItems.loot_case)
+                .patternLine("SES")
+                .patternLine("ECE")
+                .patternLine("SES")
+                .setGroup("mystictools:charm")
+                .addCriterion("has_item", hasItem(ModItems.shard))
+                .build(consumer);
+        ShapedRecipeBuilder.shapedRecipe(ModItems.dice)
+                .key('D', Blocks.DIAMOND_BLOCK)
+                .key('E', Blocks.EMERALD_BLOCK)
+                .key('C', ModItems.loot_case)
+                .patternLine("DCD")
+                .patternLine("CEC")
+                .patternLine("DCD")
+                .setGroup("mystictools:dice")
+                .addCriterion("has_item", hasItem(ModItems.loot_case))
                 .build(consumer);
     }
 
