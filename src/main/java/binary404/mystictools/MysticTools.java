fee7e182c563df4b6c055037f3b38ad9bd8bc31d
CSchlaud04 <35374899+binary404@users.noreply.github.com>
2022-09-08 16:38:18 -0500

    update

--- src/main/java/binary404/mystictools/MysticTools.java
+++ src/main/java/binary404/mystictools/MysticTools.java
@@ -33,7 +33,6 @@ import net.minecraftforge.fml.event.lifecycle.ParallelDispatchEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.checkerframework.checker.units.qual.C;
 
 @Mod("mystictools")
 public class MysticTools {
